2c6ab1154bc3b8b2af624a91b4cf95010dd96341
Joshua Pace <joshpace@gmail.com>
2018-02-05 09:35:12 -0800

    add submit handler

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -12,11 +12,14 @@ export default class SearchBar extends Component {
     this.setState({
       term: event.target.value,
     });
-    console.log(this.state.term)
+
+  }
+  onFormSubmit = (event) => {
+    event.preventDefault();
   }
   render() {
     return (
-      <form className='input-group'>
+      <form onSubmit={this.onFormSubmit} className='input-group'>
         <input
           placeholder='Get a five day forecast in your favorite cities'
           className='form-control'
