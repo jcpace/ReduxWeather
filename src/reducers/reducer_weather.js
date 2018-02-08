5c3a186e4ba024c3b7c9731356d2965da4473c6a
Joshua Pace <joshpace@gmail.com>
2018-02-07 18:43:32 -0800

    add renderWeather func to WeatherList

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -6,6 +6,8 @@ export default function(state = [], action) {
     //return state.concat([ action.payload.data ]); same as below
     return [ action.payload.data, ...state];
     // create a new array and insert all entries in the variable state inside that array. action.payload goes in front off arr
+    default:
+    // no default
   }
   // never mutate state directly. always return a new instance of state
 
