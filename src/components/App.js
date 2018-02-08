5c294045b31cb0432b3a88a7e4e0015b6b3c02cb
Joshua Pace <joshpace@gmail.com>
2018-02-07 18:18:01 -0800

    add table headings

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 
 import './App.css';
 import SearchBar from '../containers/search_bar';
+import WeatherList from '../containers/weather_list';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
 }
 
-export default App;
+
