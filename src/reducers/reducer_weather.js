450338926e8534eaebcf8c67f65c03ef1e128fd0
Joshua Pace <joshpace@gmail.com>
2018-02-07 18:09:41 -0800

    add WeatherList container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,8 +3,11 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-    return [ action.payload.data ]; 
+    //return state.concat([ action.payload.data ]); same as below
+    return [ action.payload.data, ...state];
+    // create a new array and insert all entries in the variable state inside that array. action.payload goes in front off arr
   }
+  // never mutate state directly. always return a new instance of state
 
   return state;
 }
