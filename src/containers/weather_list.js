842da3d642c71ff8e3ca0087f4c764e38ca5cabc
Joshua Pace <joshpace@gmail.com>
2018-02-12 16:08:23 -0800

    add style to map and pass props to map component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -9,6 +10,9 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp)
     const pressures = cityData.list.map(weather => weather.main.pressure)
     const humidities = cityData.list.map(weather => weather.main.humidity)
+    const {lon, lat} = cityData.city.coord;
+    
+
    
     return (
       <tr key={name}>
@@ -26,7 +30,7 @@ class WeatherList extends Component {
       <table className='table table-hover'>
         <thead>
           <tr>
-            <th>City</th>
+            <th><GoogleMap lon={lon} lat={lat} /></th>
             <th>Temperature (K)</th>
             <th>Pressure (hPa)</th>
             <th>Humidity (%)</th>
